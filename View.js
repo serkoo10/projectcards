f6356b7d7325bd5e4bca44ac7f957ea51094a7e8
Oskar <oskar.stud@gmail.com>
2022-07-08 14:52:16 +0300

    Pre release 0.2

--- View.js
+++ View.js
@@ -1,9 +1,29 @@
+
+
 class View {
   constructor() {
 
   }
 }
 
-module.exports = View
+const readline = require("readline");
+const { stdin: input, stdout: output } = require("process");
+const rl = readline.createInterface({ input, output });
+let i = 0;
+const question = (text) =>
+new Promise((res, rej) =>
+rl.question(text, (answer) => {
+res(answer);
+})
+);
+question("как дела\n")
+.then((data) => {
+i++;
+console.log("\nyou said ", data);
+})
 
-console.log('123');
+.then(() => rl.close());
+
+
+
+module.exports = View
